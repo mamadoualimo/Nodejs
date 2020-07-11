687eebe41bb993fe1730e0122682ad2176332ba9
mamadoualimo <58432140+mamadoualimo@users.noreply.github.com>
2020-07-11 19:01:53 +0200

    copier un fichier

--- copy.js
+++ copy.js
@@ -5,12 +5,16 @@ fs.stat(file, (err, stat) => {
     let total = stat.size
     let progress = 0
     let read = fs.createReadStream(file)
+    let write = fs.createWriteStream('copy.mp4')
+    let write2 = fs.createWriteStream('copy2.mp4')
     read.on('data', (chunk) => {
         progress += chunk.length
         console.log(" j'ai lu " + Math.round(100 * progress / total) + "%")
     })
-    read.on('end', () => {
-        console.log("j'ai fini de lire le fichier")
+    read.pipe(write)
+    read.pipe(write2)
+    read.on('finish', () => {
+        console.log("Le fichier a bien été copié")
     })
 })
 
